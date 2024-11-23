8ecd1a4e4c51131fa5f50d0be59fe5b590ea1348
Alberto Rodriguez <alrodriguezgar14@gmail.com>
2024-11-23 13:54:00 +0100

    Build: Added tailwindcss and implemented basic buttons to test it out

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useRef, useState, useEffect} from 'react';
+import {useRef, useState} from 'react';
 import useWindow from "./useWindow.js";
 import './App.css';
 
@@ -38,6 +38,7 @@ function App() {
         context.beginPath();
         context.moveTo(start.offsetX, start.offsetY);
         context.lineTo(end.offsetX, end.offsetY);
+        context.strokeStyle = `rgb(150, 96, 116)`;
         context.lineWidth = 5;
         context.lineCap = "round";
         context.lineJoin = "round";
@@ -58,25 +59,17 @@ function App() {
         context.putImageData(savedContexts[index], 0, 0);
     };
 
-    useEffect(() => {
-        const context = canvas.current.getContext("2d");
-        context.lineWidth = 5;
-        context.lineCap = "round";
-        context.lineJoin = "round";
-    }, []);
-
     return (
         <>
-            <div>
-                <h4>Canva</h4>
+            <div className="p-6">
                 <div>
                     {savedContexts.map((_, index) => (
-                        <button key={index} onClick={() => loadContext(index)}>
+                        <button className="p-2 m-2 bg-gray-800 hover:bg-gray-900 text-white rounded-3xl" key={index} onClick={() => loadContext(index)}>
                             Load Drawing {index + 1}
                         </button>
                     ))}
+                <button className="bg-violet-600 hover:bg-violet-700 text-white font-medium px-3 py-3 m-2 rounded shadow-2xl block border-b-4 border-r-4 border-violet-950" onClick={saveContext}>Save Drawing</button>
                 </div>
-                <button onClick={saveContext}>Save Drawing</button>
                 <canvas
                     onMouseMove={draw}
                     onMouseDown={startDrawing}
